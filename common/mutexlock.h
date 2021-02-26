3aabe9c8c5e4b36d80344fd8bc9874fe999986e9
xiaoguangcong <guangcongx@163.com>
2021-02-26 23:39:53 +0800

    add mutexlock and condition

---
+++ common/mutexlock.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <pthread.h>
+#include <cstdio>
+#include "noncopyable.h"
+
+class MutexLock : private NonCopyable
+{
+public:
+    MutexLock() 
+    {
+        pthread_mutex_init(&mutex, nullptr);
+    }
+
+    ~MutexLock()
+    {
+        pthread_mutex_lock(&mutex);
+        pthread_mutex_destroy(&mutex);
+    }
+
+    void lock()
+    {
+        pthread_mutex_lock(&mutex);
+    }
+
+    void unlock()
+    {
+        pthread_mutex_unlock(&mutex);
+    }
+
+    pthread_mutex_t* get()
+    {
+        return &mutex;
+    }
+
+private:
+    pthread_mutex_t mutex;
+
+private:
+    friend class Condition;
+
+};
+
+class MutexLockGuard : private NonCopyable
+{
+public:
+    explicit MutexLockGuard(MutexLock& mutex_) : mutex(mutex_)
+    {
+        mutex.lock();
+    }
+
+    ~MutexLockGuard()
+    {
+        mutex.unlock();
+    }
+
+private:
+    MutexLock &mutex;
+};
---
+++ common/noncopyable.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class NonCopyable
+{
+protected:
+    NonCopyable() {}
+    ~NonCopyable() {}
+private:
+    NonCopyable(const NonCopyable&);
+    const NonCopyable& operator=(const NonCopyable&);
+};
