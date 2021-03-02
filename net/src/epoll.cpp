cd8a24738ee0d6a7d4ba913f151c167b685a34b8
xiaoguangcong <guangcongx@163.com>
2021-03-02 08:08:26 +0800

    add epoll

--- net/src/epoll.cpp
+++ net/src/epoll.cpp
@@ -2,42 +2,140 @@
 // Created by xgc on 2020/10/8.
 //
 
-#include "epoll.h"
+#include "../include/epoll.h"
+#include "../../base/include/logging.h"
 
+#include <cassert>
+#include <cstdio>
 #include <iostream>
+#include <memory>
+#include <sys/epoll.h>
 #include <unistd.h> // unistd.h 中所定义的接口通常都是大量针对系统调用的封装, 如 fork、pipe 以及各种 I/O 原语（read、write、close 等等）
+#include <vector>
 
-Epoll::Epoll() : epoll_fd(epoll_create(MAXEVENTS)), event_list(MAXEVENTS) {}
+const int static EVENTS_SUM = 4096;
+const int EPOLLWAIT_TIME = 10000;
 
-Epoll::~Epoll() { close(epoll_fd); }
+//typedef std::shared_ptr<Channel> ChannelPtr;
 
-int Epoll::add(int fd, void *ptr, int events) {
-  struct epoll_event event;
-  event.data.ptr = ptr;
-  event.events = events;
-  return epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event);
+Epoll::Epoll() : epoll_fd_(epoll_create1(EPOLL_CLOEXEC)), events_(EVENTS_SUM) 
+{
+  assert(epoll_fd_ > 0);
 }
 
-int Epoll::mod(int fd, void *ptr, int events) {
-  struct epoll_event event;
-  event.data.ptr = ptr;
-  event.events = events;
-  return epoll_ctl(epoll_fd, EPOLL_CTL_MOD, fd, &event);
-}
+Epoll::~Epoll() {}
 
-int Epoll::del(int fd, void *ptr, int events) {
-  struct epoll_event event;
-  event.data.ptr = ptr;
-  event.events = events;
-  return epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, &event);
-}
-
-// Need to fix
-int Epoll::wait(int timeouts) {
-  int event_nums = epoll_wait(epoll_fd, event_list.data(),
-                              static_cast<int>(event_list.size()), timeouts);
-  if (event_nums < 0) {
-    std::cout << "[Epoll::wait(): events error]" << std::endl;
+// 注册新描述符
+void Epoll::Add( ChannelPtr request, int timeout) {
+  int fd = request->GetFd();
+  if (timeout > 0)
+  {
+    AddTimer(request, timeout);
+    fd2channel_[fd] = request->GetHolder();
   }
-  return event_nums;
+  struct epoll_event event;
+  event.data.fd = fd;
+  event.events = request->GetEvents();
+  request->EqualAndUpdateLastEvents();
+
+  fd2channel_[fd] = request;
+  if (epoll_ctl(fd, EPOLL_CTL_ADD, fd, &event) < 0)
+  {
+    perror("epoll add error\n");
+    fd2channel_->reset();
+  }
+}
+
+void Epoll::Mod(ChannelPtr request, int timeout) {
+  if (timeout > 0)
+  {
+    AddTimer(request, timeout);
+  }
+  int fd = request->GetFd();
+  if (!request->EqualAndUpdateLastEvents())
+  {
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = request->GetEvents();
+    if (epoll_ctl(fd, EPOLL_CTL_MOD, fd, &event) < 0) 
+    {
+      perror("epoll mod error");
+      fd2channel_[fd].reset();
+    }
+  }
+}
+
+void Epoll::Del(ChannelPtr request) {
+  int fd = request->GetFd();
+  struct epoll_event event;
+  event.data.fd = fd;
+  event.events = request->GetLastEvents();
+  if (epoll_ctl(fd, EPOLL_CTL_DEL, fd, &event) < 0) {
+    perror("epoll del error");
+  }
+  fd2channel_[fd].reset();
+  fd2http_[fd].reset();
+}
+
+// 返回活跃事件数
+std::vector<ChannelPtr> Epoll::Poll()
+{
+  while (true)
+  {
+    int event_count = 
+      epoll_wait(epoll_fd_, &*events_.begin(), events_.size(), EPOLLWAIT_TIME);
+    if (event_count < 0)
+    {
+      perror("epoll wait error");
+    }
+    std::vector<ChannelPtr> req_data = GetEventRequest(event_count);
+    if (req_data.size() > 0)
+    {
+      return req_data;
+    }
+  }
+}
+
+
+void Epoll::HandleExpired()
+{
+  timer_manager_.HandleExpiredEvent();
+}
+
+// 分发处理函数
+std::vector<ChannelPtr> Epoll::GetEventRequest(int events_num)
+{
+  std::vector<ChannelPtr> req_data;
+  for(int i = 0; i < events_num; ++i)
+  {
+    // 获取有事件产生的描述符
+    int fd = events_[i].data.fd;
+
+    ChannelPtr cur_req = fd2channel_[fd];
+
+    if (cur_req)
+    {
+      cur_req->SetRevents(events_[i].events);
+      cur_req->SetEvents(0);
+      req_data.push_back(cur_req);
+    }
+    else 
+    {
+      LOG << "cur_req ptr is invalid";
+    }
+  }
+  return req_data;
+}
+
+void Epoll::AddTimer(ChannelPtr request_data, int timeout)
+{
+  std::shared_ptr<HttpData> http_data = request_data->GetHolder();
+  if (http_data)
+  {
+    timer_manager_.AddTimer(http_data, timeout);
+  }
+  else
+  {
+    LOG << "timer add fail";
+  }
 }
